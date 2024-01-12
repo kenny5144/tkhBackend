b222b7f5ef7e832a0a34ef102d0905a04e4c6ea3
kehinde ojo <ojok761@gmail.com>
2024-01-11 22:00:49 -0500

    made a new branch and did the next part of the crud application

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("is this on");
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
@@ -8,12 +9,16 @@ const PORT = 3000;
 // 	console.log(`Server is live! Listening at port ${PORT}`);
 // 	// if this does not work, remember to save your file after each major change
 // })
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 app.get('/', function (req, res){
 	res.sendFile(__dirname + '/index.html');
 	// if this does not work, remember to save your file after each major change
 })
 
+app.post('/users', (req, res) => {
+	console.log(req.body)
+})
 app.listen(PORT, function() {
 	console.log(`Server is live! Listening at port ${PORT}`);
 })
