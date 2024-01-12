0b1d65a5a6eeb128ee6fcae05d18c1a2ac7b24bf
kehinde ojo <ojok761@gmail.com>
2024-01-11 21:48:03 -0500

    adding all the files to git hub

---
+++ server.js
@@ -0,0 +1,19 @@
+console.log("is this on");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+// app.listen(PORT, function() {
+// 	console.log(`Server is live! Listening at port ${PORT}`);
+// 	// if this does not work, remember to save your file after each major change
+// })
+
+app.get('/', function (req, res){
+	res.sendFile(__dirname + '/index.html');
+	// if this does not work, remember to save your file after each major change
+})
+
+app.listen(PORT, function() {
+	console.log(`Server is live! Listening at port ${PORT}`);
+})
