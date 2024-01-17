83718b0fa74fdf4ec4787215a7c618648bc5fc02
kehinde ojo <ojok761@gmail.com>
2024-01-16 19:38:56 -0500

    final part done

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 console.log("is this on");
-const express = require('express');
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 const PORT = 3000;
-
+require("dotenv").config({ path: ".env" });
 
 // app.listen(PORT, function() {
 // 	console.log(`Server is live! Listening at port ${PORT}`);
 // 	// if this does not work, remember to save your file after each major change
 // })
-app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get('/', function (req, res){
-	res.sendFile(__dirname + '/index.html');
-	// if this does not work, remember to save your file after each major change
-})
+MongoClient.connect(process.env.MONGO_URL)
+  .then((client) => {
+    const db = client.db("practice");
+    const usersCollection = db.collection("users");
+    app.get('/', (req, res) => {
+      usersCollection
+          .find()
+          .toArray()
+          .then(results => {
+              res.render('index.ejs', { usersCollection: results })
+          })
+          .catch(error => console.error(error))
+  })
+    app.post("/users", (req, res) => {
+      usersCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.log(error));
+    });
+  })
+  .catch((error) => console.error(error));
+
+// app.get('/', function (req, res){
+// 	res.sendFile(__dirname + '/index.html');
+// 	// if this does not work, remember to save your file after each major change
+// })
 
 app.post('/users', (req, res) => {
 	console.log(req.body)
